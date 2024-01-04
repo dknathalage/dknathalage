2e21c4d196b68e71d985c952f9657caef6ddc920
Don Athalage <dknathalage@gmail.com>
2024-01-04 11:25:38 +1100

    change popup to redirect

--- web/src/lib/firebase/client/authentication.ts
+++ web/src/lib/firebase/client/authentication.ts
@@ -1,11 +1,10 @@
 import { auth } from '$lib/firebase/client/config';
 // import { user } from "$lib/stores/user" auth state changes here when users are logged in from here
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithRedirect, signOut } from 'firebase/auth';
 
 export const signin = async function () {
 	const provider = new GoogleAuthProvider();
-	const creds = await signInWithPopup(auth, provider);
-	console.log(creds);
+	await signInWithRedirect(auth, provider);
 };
 
 export const signout = async function () {
