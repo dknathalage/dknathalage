a1df6d7c9cb3d5308fbf9524430bcc668d41df04
Don Athalage <dknathalage@gmail.com>
2024-01-06 22:01:42 +1100

    TopNav complete

--- web/src/routes/app/+layout.svelte
+++ web/src/routes/app/+layout.svelte
@@ -2,17 +2,20 @@
 	import { signout } from '$lib/firebase/client/auth';
 	import { auth } from '$lib/firebase/client/config';
 	import Authenticated from '$lib/components/Authenticated.svelte';
+	import { writable } from 'svelte/store';
+
+	const active = writable('home');
 </script>
 
 <Authenticated>
-	<div class="flex-1 navbar bg-base-100 fixed bottom-0 sm:bottom-auto">
-		<div class="flex-1 hidden sm:block">
+	<div class="flex-1 navbar bg-base-100 fixed bottom-0 sm:bottom-auto justify-center">
+		<div class="md:flex-1 hidden sm:block">
 			<a class="btn btn-ghost text-lg"> [Business Name] </a>
 		</div>
-		<div class="flex items-center">
+		<div class="items-center">
 			<ul class="menu bg-base-200 menu-horizontal rounded-box">
 				<li>
-					<a>
+					<a class:active={$active === 'home'} on:click={() => active.set('home')}>
 						<svg
 							class="w-5 h-5"
 							aria-hidden="true"
@@ -35,7 +38,49 @@
 					</a>
 				</li>
 				<li>
-					<a>
+					<a class:active={$active === 'people'} on:click={() => active.set('people')}>
+						<svg
+							class="w-5 h-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 20 20"
+						>
+							<path
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M4.333 6.764a3 3 0 1 1 3.141-5.023M2.5 16H1v-2a4 4 0 0 1 4-4m7.379-8.121a3 3 0 1 1 2.976 5M15 10a4 4 0 0 1 4 4v2h-1.761M13 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-4 6h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z"
+							/>
+						</svg>
+						<div class="hidden md:block">People</div>
+					</a>
+				</li>
+				<li>
+					<a class:active={$active === 'transactions'} on:click={() => active.set('transactions')}>
+						<svg
+							class="w-5 h-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							viewBox="0 0 19 20"
+						>
+							<path
+								d="M18.972.863a.913.913 0 0 0-.041-.207.956.956 0 0 0-.107-.19 1.01 1.01 0 0 0-.065-.116c-.008-.01-.02-.013-.028-.022a1.008 1.008 0 0 0-.174-.137 1.085 1.085 0 0 0-.141-.095 1.051 1.051 0 0 0-.171-.047.985.985 0 0 0-.207-.041C18.025.007 18.014 0 18 0h-3.207a1 1 0 1 0 0 2h.5l-4.552 3.9-3.5-.874a1 1 0 0 0-.867.189l-5 4a1 1 0 0 0 1.25 1.562L7.238 7.09l3.52.88a1 1 0 0 0 .892-.211L17 3.173v1.034a1 1 0 0 0 2 0V1a.9.9 0 0 0-.028-.137ZM13.5 9a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11Zm.24 4.591a3.112 3.112 0 0 1 1.935 1.374 2.036 2.036 0 0 1 .234 1.584 2.255 2.255 0 0 1-1.374 1.469.982.982 0 0 1-1.953.09 2.943 2.943 0 0 1-1.475-.92 1 1 0 0 1 1.536-1.283.953.953 0 0 0 .507.29.778.778 0 0 0 .831-.18 1.108 1.108 0 0 0-.714-.481 3.105 3.105 0 0 1-1.934-1.374 2.042 2.042 0 0 1-.233-1.584 2.264 2.264 0 0 1 1.45-1.493v-.03a1 1 0 0 1 2 0c.517.159.98.457 1.337.862a1.002 1.002 0 1 1-1.524 1.3.962.962 0 0 0-.507-.286.775.775 0 0 0-.829.18 1.113 1.113 0 0 0 .713.482ZM6 20a1 1 0 0 1-1-1v-6a1 1 0 1 1 2 0v6a1 1 0 0 1-1 1Zm-4 0a1 1 0 0 1-1-1v-4a1 1 0 1 1 2 0v4a1 1 0 0 1-1 1Z"
+							/>
+						</svg>
+						<div class="hidden md:block">
+							Transactions
+							<span class="badge badge-sm badge-success">+99</span>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a
+						class:active={$active === 'notifications'}
+						on:click={() => active.set('notifications')}
+					>
 						<svg
 							class="w-5 h-5"
 							aria-hidden="true"
@@ -52,32 +97,13 @@
 							/>
 						</svg>
 						<div class="hidden md:block">
-							Updates
+							Notifications
 							<span class="badge badge-sm badge-warning">NEW</span>
 						</div>
 					</a>
 				</li>
 				<li>
-					<a>
-						<svg
-							class="w-5 h-5"
-							aria-hidden="true"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="currentColor"
-							viewBox="0 0 19 20"
-						>
-							<path
-								d="M18.972.863a.913.913 0 0 0-.041-.207.956.956 0 0 0-.107-.19 1.01 1.01 0 0 0-.065-.116c-.008-.01-.02-.013-.028-.022a1.008 1.008 0 0 0-.174-.137 1.085 1.085 0 0 0-.141-.095 1.051 1.051 0 0 0-.171-.047.985.985 0 0 0-.207-.041C18.025.007 18.014 0 18 0h-3.207a1 1 0 1 0 0 2h.5l-4.552 3.9-3.5-.874a1 1 0 0 0-.867.189l-5 4a1 1 0 0 0 1.25 1.562L7.238 7.09l3.52.88a1 1 0 0 0 .892-.211L17 3.173v1.034a1 1 0 0 0 2 0V1a.9.9 0 0 0-.028-.137ZM13.5 9a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11Zm.24 4.591a3.112 3.112 0 0 1 1.935 1.374 2.036 2.036 0 0 1 .234 1.584 2.255 2.255 0 0 1-1.374 1.469.982.982 0 0 1-1.953.09 2.943 2.943 0 0 1-1.475-.92 1 1 0 0 1 1.536-1.283.953.953 0 0 0 .507.29.778.778 0 0 0 .831-.18 1.108 1.108 0 0 0-.714-.481 3.105 3.105 0 0 1-1.934-1.374 2.042 2.042 0 0 1-.233-1.584 2.264 2.264 0 0 1 1.45-1.493v-.03a1 1 0 0 1 2 0c.517.159.98.457 1.337.862a1.002 1.002 0 1 1-1.524 1.3.962.962 0 0 0-.507-.286.775.775 0 0 0-.829.18 1.113 1.113 0 0 0 .713.482ZM6 20a1 1 0 0 1-1-1v-6a1 1 0 1 1 2 0v6a1 1 0 0 1-1 1Zm-4 0a1 1 0 0 1-1-1v-4a1 1 0 1 1 2 0v4a1 1 0 0 1-1 1Z"
-							/>
-						</svg>
-						<div class="hidden md:block">
-							Payments
-							<span class="badge badge-sm badge-success">+99</span>
-						</div>
-					</a>
-				</li>
-				<li>
-					<a>
+					<a class:active={$active === 'preferences'} on:click={() => active.set('preferences')}>
 						<svg
 							class="w-5 h-5"
 							aria-hidden="true"
@@ -97,7 +123,7 @@
 					</a>
 				</li>
 			</ul>
-			<ul class="menu bg-base-200 lg:menu-horizontal rounded-box ml-2">
+			<ul class="menu bg-base-200 lg:menu-horizontal rounded-box ml-2 hidden md:block">
 				<li>
 					<a
 						on:click={() => {
