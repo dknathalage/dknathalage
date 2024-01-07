8ca47a306db7fa38377172c13cf83356795a940a
Don Athalage <dknathalage@gmail.com>
2024-01-07 20:07:11 +1100

    remove people cards

--- src/routes/app/people/+page.svelte
+++ src/routes/app/people/+page.svelte
@@ -1,18 +1,1 @@
-<div class="grid lg:grid-flow-col lg:grid-col-2 gap-5">
-	<div class="card bg-base-200">
-		<div class="card-body">
-			<h2 class="card-title">Team</h2>
-			Hello THere
-			<div class="card-actions justify-end">
-				<button class="btn btn-secondary">Buy Now</button>
-				<button class="btn btn-primary">Buy Now</button>
-			</div>
-		</div>
-	</div>
-	<div class="card bg-base-200">
-		<div class="card-body">
-			<h2 class="card-title">Clients</h2>
-			Hello There
-		</div>
-	</div>
-</div>
+
