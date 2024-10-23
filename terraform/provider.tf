8aad634b5fb5100ba000364ecc0acb95d9abe651
dknathalage <dknathalage@gmail.com>
2024-10-23 22:22:30 +1100

    add google login to terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,7 +4,18 @@ terraform {
       source  = "vultr/vultr"
       version = "2.21.0"
     }
+
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
   }
+}
+
+provider "google" {
+  project = "dknathalage"
+  region  = "australia-southeast2"
+
 }
 
 provider "vultr" {
