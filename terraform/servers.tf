1f6e7d2db3861a3edd62608aaec8981dff766b22
dknathalage <dknathalage@gmail.com>
2024-10-24 22:07:49 +1100

    add ssh key to instance

--- terraform/servers.tf
+++ terraform/servers.tf
@@ -1,6 +1,7 @@
 resource "vultr_instance" "test_instance" {
-  plan     = "vc2-1c-1gb"
-  region   = "mel"
-  image_id = "docker"
-  label    = "test-instance"
+  plan        = "vc2-1c-1gb"
+  region      = "mel"
+  image_id    = "docker"
+  label       = "test-instance"
+  ssh_key_ids = [vultr_ssh_key.dkn-ssh-key.id]
 }
