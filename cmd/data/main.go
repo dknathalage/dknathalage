4d060a7617276ac17f9ce4598439c2ce5e5dca25
dknathalage <dknathalage@gmail.com>
2024-10-25 22:04:00 +1100

    add ssh action

--- cmd/data/main.go
+++ cmd/data/main.go
@@ -12,5 +12,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Starting server on :8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 }
