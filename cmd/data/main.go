db526dc49ba536167e9e462af1e6a35472ec08cc
dknathalage <dknathalage@gmail.com>
2024-10-30 21:58:53 +1100

    Refactor CI workflows and task commands
    - Renamed the "go-lint" task to "lint" in the CI workflows.
    - Renamed the "go-test" task to "test" in the CI workflows.
    - Renamed the "go-sec" task to "sec" in the CI workflows.
    - Updated the Taskfile.yml to reflect the task command changes.
    - Removed the unused "pkg/app/app.go" file.

--- cmd/data/main.go
+++ cmd/data/main.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// App encapsulates application dependencies.
+// This is useful for injecting dependencies in tests.
+type App struct{}
+
+// HelloHandler responds with a greeting message.
+func (a *App) HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Docker!")
 }
 
+// NewServer configures and returns an http.Server.
+func NewServer(addr string, handler http.Handler) *http.Server {
+	return &http.Server{
+		Addr:              addr,
+		Handler:           handler,
+		ReadHeaderTimeout: 3 * time.Second,
+	}
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	fmt.Println("Starting server on :8080")
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
-		fmt.Println("Error starting server: ", err)
+	app := &App{}
+
+	// Create a new ServeMux and register handlers.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.HelloHandler)
+
+	// Define the server address.
+	serverAddr := ":80"
+	fmt.Printf("Starting server on %s\n", serverAddr)
+
+	// Initialize the server.
+	server := NewServer(serverAddr, mux)
+
+	// Start the server.
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Error starting server: %v", err)
 	}
 }
