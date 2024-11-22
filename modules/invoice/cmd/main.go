9ebf53ef5a94dffa81d419edfc777f4ce97f1739
dknathalage <dknathalage@gmail.com>
2024-11-23 08:37:16 +1100

    deploy cloudrun

--- modules/invoice/cmd/main.go
+++ modules/invoice/cmd/main.go
@@ -8,21 +8,15 @@ import (
 )
 
 func main() {
-	// Echo instance
 	e := echo.New()
 
-	// Middleware
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-
-	// Routes
 	e.GET("/", hello)
 
-	// Start server
 	e.Logger.Fatal(e.Start(":80"))
 }
 
-// Handler
 func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
