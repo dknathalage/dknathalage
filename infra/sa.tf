9db746f6fdfaef29b95909f45dfee9e85e3509bd
dknathalage <dknathalage@gmail.com>
2024-11-25 19:21:42 +1100

    update

--- infra/sa.tf
+++ infra/sa.tf
@@ -1,10 +1,16 @@
 
 resource "google_service_account" "actions_sa" {
-  account_id   = "gha-ci-sa"
+  account_id = "gha-ci-sa"
 }
 
 resource "google_service_account_iam_member" "actions_sa_member" {
   service_account_id = "projects/dknathalage/serviceAccounts/${google_service_account.actions_sa.email}"
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.dkn_identity_pool.name}/*"
+}
+
+resource "google_service_account_iam_member" "editor_permissions" {
+  service_account_id = "projects/dknathalage/serviceAccounts/${google_service_account.actions_sa.email}"
+  role               = "roles/editor"
+  member             = "serviceAccount:${google_service_account.actions_sa.email}"
 }
