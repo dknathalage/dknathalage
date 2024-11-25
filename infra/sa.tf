f63c96cc71b9ed7133bee4a9f514645d8b1581c5
dknathalage <dknathalage@gmail.com>
2024-11-25 18:27:33 +1100

    import owner binding

--- infra/sa.tf
+++ infra/sa.tf
@@ -2,7 +2,21 @@ resource "google_service_account" "service_account" {
   account_id = "gha-ci-sa"
 }
 
+resource "google_service_account_iam_binding" "service_account_iam_binding" {
+  service_account_id = google_service_account.service_account.email
+  role               = "roles/owner"
+
+  members = [
+    "serviceAccount: ${google_service_account.service_account.email}",
+  ]
+}
+
 import {
   id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com"
   to = google_service_account.service_account
+}
+
+import {
+  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com roles/owner"
+  to = google_service_account_iam_binding.service_account_iam_binding
 }
