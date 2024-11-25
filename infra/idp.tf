16e77dfbca2bb80ab1470085bf03fc49fa25d29b
dknathalage <dknathalage@gmail.com>
2024-11-25 19:14:01 +1100

    push changes

---
+++ infra/idp.tf
@@ -0,0 +1,21 @@
+resource "google_iam_workload_identity_pool" "dkn_identity_pool" {
+  workload_identity_pool_id = "id-pool"
+}
+
+resource "google_iam_workload_identity_pool_provider" "gha_provider" {
+  project                            = "dknathalage"
+  workload_identity_pool_id          = google_iam_workload_identity_pool.dkn_identity_pool.workload_identity_pool_id
+  workload_identity_pool_provider_id = "github-actions"
+  attribute_condition                = "attribute.repository_owner==\"dknathalage\""
+  attribute_mapping = {
+    "google.subject"             = "assertion.sub"
+    "attribute.actor"            = "assertion.actor"
+    "attribute.aud"              = "assertion.aud"
+    "attribute.repository"       = "assertion.repository"
+    "attribute.repository_owner" = "assertion.repository_owner"
+  }
+  oidc {
+    allowed_audiences = []
+    issuer_uri        = "https://token.actions.githubusercontent.com"
+  }
+}
--- infra/sa.tf
+++ infra/sa.tf
@@ -1,21 +1,10 @@
-resource "google_service_account" "gha_ci_sa" {
-  account_id = "gha-ci-sa"
+
+resource "google_service_account" "actions_sa" {
+  account_id   = "gha-ci-sa"
 }
 
-resource "google_service_account_iam_binding" "workload_identity_user" {
-  service_account_id = google_service_account.gha_ci_sa.name
+resource "google_service_account_iam_member" "actions_sa_member" {
+  service_account_id = "projects/dknathalage/serviceAccounts/${google_service_account.actions_sa.email}"
   role               = "roles/iam.workloadIdentityUser"
-
-  members = [
-    "principalSet://iam.googleapis.com/projects/719430876063/locations/global/workloadIdentityPools/dknathalage-identity-pool/attribute.repository/dknathalage/dknathalage"
-  ]
-}
-
-resource "google_service_account_iam_binding" "owner" {
-  service_account_id = google_service_account.gha_ci_sa.name
-  role               = "roles/owner"
-
-  members = [
-    "serviceAccount:gha-ci-sa@dknathalage.iam.gserviceaccount.com"
-  ]
+  member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.dkn_identity_pool.name}/*"
 }
