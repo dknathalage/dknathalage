2477bb7f0c3ecd071e307759f6f597dae696b4f5
dknathalage <dknathalage@gmail.com>
2024-11-25 18:34:42 +1100

    add bindings

--- infra/sa.tf
+++ infra/sa.tf
@@ -2,12 +2,21 @@ resource "google_service_account" "service_account" {
   account_id = "gha-ci-sa"
 }
 
-resource "google_service_account_iam_binding" "service_account_iam_binding" {
-  service_account_id = google_service_account.service_account.email
+resource "google_service_account_iam_binding" "workload_identity_user" {
+  service_account_id = google_service_account.gha_ci_sa.name
+  role               = "roles/iam.workloadIdentityUser"
+
+  members = [
+    "principalSet://iam.googleapis.com/projects/719430876063/locations/global/workloadIdentityPools/dknathalage-identity-pool/attribute.repository/dknathalage/dknathalage"
+  ]
+}
+
+resource "google_service_account_iam_binding" "owner" {
+  service_account_id = google_service_account.gha_ci_sa.name
   role               = "roles/owner"
 
   members = [
-    "serviceAccount: projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com"
+    "serviceAccount:gha-ci-sa@dknathalage.iam.gserviceaccount.com"
   ]
 }
 
@@ -17,6 +26,11 @@ import {
 }
 
 import {
-  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com roles/owner"
-  to = google_service_account_iam_binding.service_account_iam_binding
+  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com roles/iam.workloadIdentityUser"
+  to = google_service_account_iam_binding.workload_identity_user
+}
+
+import {
+  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com roles/iam.workloadIdentityUser"
+  to = google_service_account_iam_binding.owner
 }
