be092b698b04c7e9f1bdde4681268b6107c10e8a
dknathalage <dknathalage@gmail.com>
2024-11-26 21:22:56 +1100

    read all access

--- modules/semver/cmd/main.go
+++ modules/semver/cmd/main.go
@@ -35,10 +35,8 @@ func main() {
 		return
 	}
 
-	// Rate.Limit should most likely be 5000 when authorized.
 	log.Printf("Rate: %#v\n", resp.Rate)
 
-	// If a Token Expiration has been set, it will be displayed.
 	if !resp.TokenExpiration.IsZero() {
 		log.Printf("Token Expiration: %v\n", resp.TokenExpiration)
 	}
