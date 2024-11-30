4c7b2276044a18f089c2fdc9d9ddfac2199a8662
dknathalage <dknathalage@gmail.com>
2024-12-01 08:48:24 +1100

    completed imports

--- infra/shared/sa.tf
+++ infra/shared/sa.tf
@@ -9,15 +9,25 @@ resource "google_service_account_iam_member" "actions_sa_member" {
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.dkn_identity_pool.name}/*"
 }
 
-import {
-  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com"
-  to = google_service_account.actions_sa
-}
-
 resource "google_project_iam_binding" "actions_sa_binding" {
   project = "dknathalage"
   role    = "roles/editor"
   members = [
     "serviceAccount:${google_service_account.actions_sa.email}"
   ]
+}
+
+import {
+  to = google_service_account.actions_sa
+  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com"
+}
+
+import {
+  to = google_service_account_iam_member.actions_sa_member
+  id = "projects/dknathalage/serviceAccounts/gha-ci-sa@dknathalage.iam.gserviceaccount.com roles/iam.workloadIdentityUser principalSet://iam.googleapis.com/projects/719430876063/locations/global/workloadIdentityPools/id-pool/*"
+}
+
+import {
+  to = google_project_iam_binding.actions_sa_binding
+  id = "dknathalage roles/editor serviceAccount:gha-ci-sa@dknathalage.iam.gserviceaccount.com"
 }
