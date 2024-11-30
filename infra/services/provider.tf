267834af06e6a5e1f9046aaef2bf5a1b61a9bdbc
dknathalage <dknathalage@gmail.com>
2024-12-01 07:54:23 +1100

    git push diff

---
+++ infra/services/provider.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project = "dknathalage"
+  region  = "australia-southeast2"
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.12.0"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "dknathalage-tfstate"
+    prefix = "terraform/state"
+  }
+}
