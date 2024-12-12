98300cbe34cc9011c1e095cc66aa50691b3ac291
dknathalage <dknathalage@gmail.com>
2024-12-13 07:47:57 +1100

    add cleanup policies

--- infra/aritfact-registry.tf
+++ infra/aritfact-registry.tf
@@ -12,6 +12,33 @@ resource "google_artifact_registry_repository" "docker-artifacts" {
   location      = each.value.location
   repository_id = each.value.repository_id
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete"
+    action = "DELETE"
+    condition {
+      older_than            = "3600s"
+      package_name_prefixes = []
+      tag_prefixes          = []
+      tag_state             = "ANY"
+      version_name_prefixes = []
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep"
+    action = "KEEP"
+    condition {
+      tag_prefixes          = ["refs-"]
+      tag_state             = "TAGGED"
+      package_name_prefixes = []
+      version_name_prefixes = []
+    }
+  }
+
+  docker_config {
+    immutable_tags = false
+  }
 }
 
 import {
