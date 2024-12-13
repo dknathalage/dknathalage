13e66d931df416668e4b619649304c1d6c1707e8
dknathalage <dknathalage@gmail.com>
2024-12-14 06:33:36 +1100

    delete default network

--- infra/gcp-project.tf
+++ infra/gcp-project.tf
@@ -10,6 +10,7 @@ resource "google_project" "my_project" {
   for_each   = var.project_map
   name       = each.key
   project_id = each.value.project_id
+  auto_create_network = false
 }
 
 import {
