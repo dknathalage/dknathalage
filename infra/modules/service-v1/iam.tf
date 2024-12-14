23208be7227ecf54f52f9840e8aeea3b067f20b6
dknathalage <dknathalage@gmail.com>
2024-12-15 10:40:47 +1100

    change name

--- infra/modules/service-v1/iam.tf
+++ infra/modules/service-v1/iam.tf
@@ -4,6 +4,6 @@ variable "sa_roles" {
 }
 
 resource "google_service_account" "service_account" {
-  account_id   = "cloudrun-sa-${var.name}"
+  account_id   = "${var.name}-sa"
   display_name = "CloudRun Service Account for ${var.name}"
 }
