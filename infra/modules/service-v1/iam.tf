5269a5ee1035559e53a5d71af3b05e7f14f17d79
dknathalage <dknathalage@gmail.com>
2024-12-15 11:12:01 +1100

    add artifact registry iam member to cloudrun sa

--- infra/modules/service-v1/iam.tf
+++ infra/modules/service-v1/iam.tf
@@ -7,3 +7,11 @@ resource "google_service_account" "service_account" {
   account_id   = "${var.name}-sa"
   display_name = "CloudRun Service Account for ${var.name}"
 }
+
+resource "google_artifact_registry_repository_iam_member" "member" {
+  project    = var.project
+  location   = var.location
+  repository = var.artifact_registry
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_service_account.service_account.email}"
+}
